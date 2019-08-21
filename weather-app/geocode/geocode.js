821ef13c80b2652eb2b6c563ef715b322625f21b
matt-user <54373384+matt-user@users.noreply.github.com>
2019-08-21 15:40:06 -0400

    removed my api key from the file

--- weather-app/geocode/geocode.js
+++ weather-app/geocode/geocode.js
@@ -4,7 +4,7 @@ var geocodeAddress = (address, callback) => {
 	var encodedAddress = encodeURIComponent(address);
 
 	request({
-		url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=AIzaSyCCz-Qg-IswjQ3LT7tclA99vP9SNcEY6tg`,
+		url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}&key=apikey`,
 		json: true
 	}, (error, response, body) => {
 		if(error){
