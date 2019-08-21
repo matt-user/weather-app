7b09c8816e16d475242f26db8c910983ae16c06e
matt-user <54373384+matt-user@users.noreply.github.com>
2019-08-21 15:40:37 -0400

    removed my api key from the file

--- weather-app/weather/weather.js
+++ weather-app/weather/weather.js
@@ -2,7 +2,7 @@ const request = require('request');
 
 var getWeather = (lat, lng, callback) => {
 	request({
-		url: `https://api.darksky.net/forecast/846bc25b2cc78f43cda864c2b68640b0/${lat},${lng}`,
+		url: `https://api.darksky.net/forecast/apikey/${lat},${lng}`,
 		json: true
 	}, (error, response, body) => {
 		if (!error && response.statusCode === 200) {
